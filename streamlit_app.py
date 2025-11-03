bd9c4c138c9c2d80314d588feb42b598fdb14bd9
lucamarques1 <luca.emb@gmail.com>
2025-11-03 18:51:56 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import pandas as pd
+import matplotlib as plt
+from supabase import create_client, Client
+
+# Connect to supabase securely
+url = st.secrets["supabase"]["url"]
+key = st.secrets["supabase"]["key"]
+supabase: Client = create_client(url, key)
+
 
 st.title("💹 PerFin Dashboard")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is your personal finance tracker with integrated AI recommendations."
 )
+
+uploaded = st.sidebar.file_uploader("Upload CSV", type=["csv"])
+st.sidebar.info("Upload a CSV with columns: date, description, amount, category.")
+
+       
