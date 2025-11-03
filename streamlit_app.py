9ba77d105d5f46cf4e1c0f074b4406291c45d2d1
lucamarques1 <luca.emb@gmail.com>
2025-11-03 18:48:51 +0000

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("💹 PerFin Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
