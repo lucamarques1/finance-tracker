09ba842759ac80ddc5af686ce69afef57b6f5f93
lucamarques1 <luca.emb@gmail.com>
2025-11-08 17:01:28 +0000

    update app

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -29,6 +29,7 @@ st.info("Welcome back!")
 
 st.sidebar.title("Filters")
 
+
 #Filter for Income or Expense
 flow_type = st.sidebar.radio("Flow Type", ["All", "Income", "Expense"])
 # Filter for Time now
@@ -47,14 +48,17 @@ with col4:
 
 
 # Select Month and Year
+time_filter = st.sidebar.radio("Apply Time Filters", ["No", "Yes"])
 select_month = st.sidebar.selectbox("Month", range(1,13))
 select_year = st.sidebar.number_input("Year", min_value= 2023, max_value=3000)
 
 # Select Category
+category_filter = st.sidebar.radio("Apply Category Filters", ["No", "Yes"])
 select_category = st.sidebar.selectbox("Category", ["All", "Food", "Rent", "Transport", "Entertainment", "Shopping", "Health", "Savings", "Other"])
 
 
 # Apply Filters
+
 ## Flow Type
 query = supabase.table("transactions").select("*").eq("user_id", user_id)
 if flow_type == "Income": 
@@ -63,24 +67,58 @@ elif flow_type == "Expense":
     query = query.lt("amount", 0)
 
 ## Category
-if select_category != "All": 
-    query = query.eq("category", select_category)
+if category_filter == "Yes":
+    if select_category != "All": 
+        query = query.eq("category", select_category)
 
 ## Date
-start_date = date(select_year, select_month, 1)
-if select_month == 12:
-    end_date = date(select_year + 1, 1, 1)
-else: 
-    end_date = date(select_year, select_month + 1, 1)
+if time_filter == "Yes":
+    start_date = date(select_year, select_month, 1)
+    if select_month == 12:
+        end_date = date(select_year + 1, 1, 1)
+    else: 
+        end_date = date(select_year, select_month + 1, 1)
+
+    query = query.gte("date", str(start_date)).lt("date", str(end_date))
+
 
 ## Short Cut Buttons
+today = date.today()
+
 if all_time:
-    query = query 
+    # No filter
+    pass
 elif this_year:
-    this_year_start = str(date.today()).split("-")[0]
-    this_year_end = this_year_start + 1
+    button_start = date(today.year, 1, 1)
+    button_end = date(today.year + 1, 1, 1)
+    query = query.gte("date", str(button_start)).lt("date", str(button_end))
 elif this_month:
-    this_month_start = str(date.today()).split("-")[1]
+    button_start = date(today.year, today.month, 1)
+    if today.month == 12:
+        button_end = date(today.year + 1, 1, 1)
+    else:
+        button_end = date(today.year, today.month + 1, 1)
+    query = query.gte("date", str(button_start)).lt("date", str(button_end))
 elif this_week:
-    start_of_week = date.today() - timedelta(days=today.weekday())
-    end_of_week = start_of_week + timedelta(days=6)
+    button_start = today - timedelta(days=today.weekday())
+    button_end = button_start + timedelta(days=7)
+    query = query.gte("date", str(button_start)).lt("date", str(button_end))
+
+
+response = query.execute()
+data = pd.DataFrame(response.data)
+
+# KPIs
+
+total_income = data.loc[data["amount"] > 0, "amount"].sum()
+total_expense = data.loc[data["amount"] < 0, "amount"].sum()
+net_profit = total_income + total_expense
+
+savings_rate = float(data.loc[data["category"] == "Savings", "amount"].sum() /  - total_expense) * 100
+
+
+st.header("KPIs")
+st.info(f"Total Income: {total_income}")
+st.info(f"Total Expense: {total_expense}")
+st.info(f"Net Profit: {net_profit}")
+st.info(f"Savings Rate: {savings_rate}%")
